851c383650e162dde069540e59a5d13e633571d0
NirajVyas <nirajvyas28@hotmail.com>
2014-05-27 21:17:10 +0100

    some debuggin changes, mainly typos

--- Niraj/w9d2/planets_app/app/assets/javascripts/planets.js
+++ Niraj/w9d2/planets_app/app/assets/javascripts/planets.js
@@ -12,7 +12,7 @@ Planets.Collections.PlanetCollection = Backbone.Collection.extend({
   url: '/planets'
 });
 
-//Show a single note
+//Show a single planet
 Planets.Views.PlanetView = Backbone.View.extend({
   tagName:'div',
   events: {'click h2': 'makeHeadLineEditable', 'blur input': 'saveChanges', 'keyup input': 'keyboardSaveChanges'},
@@ -23,7 +23,7 @@ Planets.Views.PlanetView = Backbone.View.extend({
   },
 
   render: function() {
-    this.$el.html(this.template({note: this.model}));
+    this.$el.html(this.template({planet: this.model}));
     return this;
   },
 
@@ -53,24 +53,24 @@ Planets.Views.PlanetView = Backbone.View.extend({
 
 });
 
-// Show all notes.
+// Show all planets.
 Planets.Views.PlanetCollectionView = Backbone.View.extend({
   el: '#container',
   render: function() {
     var that = this;
     this.$el.empty();
-    this.collection.each(function(noteModel) {
+    this.collection.each(function(planetModel) {
       var planetView = new Planets.Views.PlanetView({model: planetModel});
-      that.$el.append(noteView.render().el);
+      that.$el.append(planetView.render().el);
     });
     return this;
   } // End render function
 });
 
-// Show the form for a new note.
+// Show the form for a new planet.
 Planets.Views.PlanetCreationView = Backbone.View.extend({
   el: '#container',
-  events: {'submit form': 'createNewNote'},
+  events: {'submit form': 'createNewPlanet'},
   template: _.template($('#tmpl_newplanet').html()),
 
   render: function() {
@@ -78,7 +78,7 @@ Planets.Views.PlanetCreationView = Backbone.View.extend({
     return this;
   },
 
-  createNewNote: function(ev) {
+  createNewPlanet: function(ev) {
     ev.preventDefault();
     var name = $('input[name="name"]').val();
     var size = $('input[name="size"]').val();
@@ -101,7 +101,7 @@ Planets.Routers.AppRouter = Backbone.Router.extend({
     var collectionView = new Planets.Views.PlanetCollectionView({collection: Planets.allPlanets});
     collectionView.render();
   },
-  createNewNote: function() {
+  createNewPlanet: function() {
     new Planets.Views.PlanetCreationView().render();
   }
 });
